cffc00a5bf7427a8fd2d00d186443bf40c9f87c7
terisuke <terisuke1115@gmail.com>
2023-05-11 22:09:07 +0900

    create new game

---
+++ index.js
@@ -0,0 +1,116 @@
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+import { FBXLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/FBXLoader.js';
+
+let camera;
+let scene;
+let renderer;
+let model;
+let clock = new THREE.Clock();
+let mixer;
+
+// 読み込み
+function yourFunction() {
+    //シーン<script type="module">
+    scene = new THREE.Scene();
+
+    //中心線の追加
+    scene.add(new THREE.AxesHelper(5))
+
+    //カメラの作成
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 5000);
+    //カメラセット
+    camera.position.x = 200;
+    camera.position.y = 300;
+    camera.position.z = 500;
+    //原点にセット
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    //レンダラー
+    renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: true
+    });
+    renderer.setClearColor(new THREE.Color(0xffffff));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+
+    // 滑らかにカメラコントローラーを制御する
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.2;
+
+    //光源
+    const dirLight = new THREE.SpotLight(0xffffff, 1.5);//color,強度
+    dirLight.position.set(-200, 300, 300);
+    dirLight.castShadow = true;
+    scene.add(dirLight);
+
+
+    //fbxファイルの読み込み
+
+    const loader = new FBXLoader();
+
+    loader.load('PPPH.fbx', function (object) {
+        object.scale.set(1, 1, 1)
+        //シーン内の特定のオブジェクトのアニメーション用のプレーヤー(アニメーションの調整)
+        mixer = new THREE.AnimationMixer(object);
+
+        //Animation Actionを生成
+        const action = mixer.clipAction(object.animations[0]);
+
+        //アニメーションを再生する
+        action.play();
+
+        //オブジェクトとすべての子孫に対してコールバックを実行
+        object.traverse((child) => {
+            if (child.isMesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+            }
+        });
+        scene.add(object);
+    },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log(error)
+        });
+
+    //レンダラー実行関数
+    function render() {
+        renderer.render(scene, camera)
+    }
+
+    //リサイズ処理
+    window.addEventListener('resize', onWindowResize);
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        render()
+    }
+
+    //アニメーション処理
+    function animate() {
+        if (mixer) {
+            mixer.update(clock.getDelta());
+        }
+
+        controls.update()
+        render()
+
+        requestAnimationFrame(animate);
+
+    }
+
+    animate()
+
+
+    //読み込んだシーンが暗いので、明るくする
+    renderer.outputEncoding = THREE.sRGBEncoding;
+
+    document.getElementById("WebGL-output").appendChild(renderer.domElement);
+}
+export default yourFunction;
---
+++ script.js
@@ -0,0 +1,140 @@
+window.addEventListener("load", () => {
+    // オーディオを取得
+    const audio = new Audio("idolのコピー.m4a");
+
+    // 再生、停止ボタン
+    const play = document.getElementById("play");
+    const pause = document.getElementById("pause");
+    const stop = document.getElementById("stop");
+
+    //初期ポイント配列
+    window.currentPoints = [100, 50, 25, 75, 0];
+    window.currentIndex = 0;
+
+    //再生中か否かの判定
+    window.playing = false;
+    // 再生機能
+    play.addEventListener("click", () => {
+        audio.play();
+        //タイムアウトのクリア
+        if (pointChangeTimeoutId) {
+            clearTimeout(pointChangeTimeoutId);
+        }
+        //ポイント割り当ての変更
+        const pointSequence = [[50, 75, 25, 100, 0],
+        [25, 75, 100, 50, 0],
+        [0, 25, 75, 50, 100],
+        [0, 25, 100, 50, 75],
+        [50, 100, 25, 75, 0]
+        ];
+        //変更のタイミング
+        const timingArray = [18500, 23000, 13700, 12300, 11500];
+        //タイミング変更機能実装
+        window.currentIndex = 0; // 追加：再生が開始されるたびに currentIndex をリセット
+        changePoints(pointSequence, timingArray);
+        //再生中
+        window.playing = true;
+    });
+
+    // 一時停止機能
+    pause.addEventListener("click", () => {
+        audio.pause();
+        //停止中
+        playing = false;
+        clearTimeout(pointChangeTimeoutId);
+    });
+    // 停止（巻き戻し）機能
+    stop.addEventListener("click", () => {
+        audio.pause();
+        // 曲の先頭に再生開始位置を戻す
+        audio.currentTime = 0;
+        //停止中
+        playing = false;
+        //スコアのリセット
+        window.score = 0;
+        //スコア表示もリセット
+        $("#score").html(score);
+        clearTimeout(pointChangeTimeoutId);
+    });
+    //再生終了時にカウントもストップ
+    audio.addEventListener("ended", () => {
+        //停止中
+        playing = false;
+    });
+});
+//初期の得点設定
+window.score = 0;
+//let playing = false;
+let pointChangeTimeoutId = null;
+
+//ポイント変更関数
+function changePoints(pointsArray, timingArray, index = 0) {
+    if (index < timingArray.length) {
+        pointChangeTimeoutId = setTimeout(() => {
+            window.currentPoints = pointsArray[index];
+            window.currentIndex = (window.currentIndex + 1) % pointsArray.length;
+            console.log('Points changed to:', window.currentPoints);
+            changePoints(pointsArray, timingArray, index + 1);
+        }, timingArray[index]);
+    }
+}
+//カーソルがhoverしているときに加点、外れたときには点数が入らないようにする設定
+document.querySelectorAll(".givePoints").forEach((element, index) => {
+    element.addEventListener("mouseover", function () {
+        console.log("givePoints");
+        if (playing) {
+            const points = window.currentPoints[(window.currentIndex + index) % window.currentPoints.length];
+            //timeoutIdがnullの時に加点する
+
+            if (!window.timeoutId) {
+                go(points);
+                startIncreasingPoints(points);
+            }
+        }
+    });
+    element.addEventListener("mouseout", stopIncreasingPoints);
+});
+
+//１秒毎に何点加点されたか表示する設定
+
+function go(points) {
+    if (playing) {
+        const currentPoints = points;
+        window.score += currentPoints;
+        $("#score").html(window.score);
+        $("#tag").text("+" + points);
+        console.log("Adding points:", points, "Total score:", window.score);
+        // 1秒後に次の go() 関数を呼び出す
+        window.timeoutId = setTimeout(() => go(points), 1000)
+    }
+    $("#tag").fadeIn({
+        duration: 700,
+        easing: "linear",
+        step: function (now, fx) {
+            $(this).css("top", -55 * now + "px");
+        }
+    }).fadeOut({
+        duration: 300,
+        step: function (now, fx) {
+            $(this).css("top", -55 * (2 - now) + "px");
+        }
+    });
+
+}
+//ポイントの増加停止設定
+function stopIncreasingPoints() {
+    clearTimeout(window.timeoutId);
+    window.timeoutId = null;
+}
+
+//1秒毎に割り振られたポイントを追加、表示する設定
+function startIncreasingPoints(points) {
+    if (!window.timeoutId) {
+        window.intervalId = setInterval(function () {
+            console.log("startincreasingpoints");
+            window.score += points;
+            $("#score").html(window.score);
+            $("#tag").text("+" + points);
+        }, 1000);
+    }
+}
